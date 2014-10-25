7724d2fb5d39cab118057198abbebe87edd1d417
xerox <ert.schneider@yahoo.de>
2014-10-25 13:58:49 +0200

    fix menu functionality

--- Liero3DMain/main.cpp
+++ Liero3DMain/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <sstream>
 
 #include <boost/exception/all.hpp>
 #include <boost/test/unit_test.hpp>
@@ -18,163 +19,213 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
-IrrlichtDevice* device;
-
-const int playerCount = 2;
-bool gameStarted = false;
-
-scene::IAnimatedMeshSceneNode* ball;
-std::vector<scene::IMeshSceneNode*> batters;
-
-gui::IGUIStaticText* status;
-gui::IGUIButton* exitButton,* startButton;
-scene::ICameraSceneNode* camera;
+struct IrrlichtHelper {
+	IrrlichtDevice* device;	//irrlicht main device
+	video::IVideoDriver* driver; //video driver
+	scene::ISceneManager* smgr; //scene manager
+	gui::IGUIEnvironment* gui; //gui
+};
 
 enum {
-	EXIT_GAME = 101,
+	QUIT_GAME = 101,
 	START_GAME = 102
 };
 
+int playerCount = 2;
+bool gameStarted = false;
 
-class MyEventReceiver : public EventReceiver {
+scene::IAnimatedMeshSceneNode* ball;
+scene::ISceneNodeAnimator* ballAnim;
+f32 ballSpeedX = 20.f;
+f32 ballSpeedY = 20.f;
+
+scene::ICameraSceneNode* camera;
+scene::ISceneNodeAnimator* camAnim;
+
+std::vector<scene::IMeshSceneNode*> batters;
+f32 batterSpeed = 50.f;
+
+gui::IGUIStaticText* status;
+gui::IGUIButton* exitButton,* startButton;
+
+
+//create pong event receiver
+class MyEventReceiver : public EventReceiver
+{
 public:
-	MyEventReceiver() : EventReceiver() {
+	MyEventReceiver() : EventReceiver(), irrH_() {
 	}
-	
+
 	virtual bool OnEvent(const irr::SEvent& event) {
 		EventReceiver::OnEvent(event);
-		
-		if(!gameStarted) {
-			if(event.EventType == EET_GUI_EVENT)
-			{
-				auto id = event.GUIEvent.Caller->getID();
+
+//if game has not yet started
+		if (!gameStarted) {
+			//check for gui events
+			if (event.EventType == EET_GUI_EVENT) {
+				//get event id
+				s32 id = event.GUIEvent.Caller->getID();
 				switch(event.GUIEvent.EventType) {
-					if(id == EXIT_GAME) {
-						device->closeDevice();
+					//if button is clicked check event id
+				case EGET_BUTTON_CLICKED:
+					if (id == QUIT_GAME) {
+						irrH_.device->closeDevice();
 						return true;
-					} else if (id == START_GAME) {
+					}
+					if (id == START_GAME) {
 						exitButton->setVisible(false);
-						startButton->setVisible(false);	
-                    // static camera
-						camera->removeAnimators();
+						startButton->setVisible(false);
+						// remove camera animator
+						camera->removeAnimator(camAnim);
+						//set new camera position
 						camera->setTarget(core::vector3df(0,-20,0));
-						camera->setPosition(core::vector3df(10,50,-90));				
+						camera->setPosition(core::vector3df(10,50,-90));
+
+						ball->setPosition(vector3df(0, -9, -45));
+						ball->removeAnimator(ballAnim);
+
+						//render cursor invisible
+						irrH_.device->getCursorControl()->setVisible(false);
+						//indicate that game starts
+						gameStarted = true;
 					}
 				}
 			}
+
+			return false;
+		} else {
+			return false;
 		}
-		return false;
 	}
+
+	void setIrrlichtHelper(IrrlichtHelper irrH) {
+		irrH_ = irrH;
+	}
+
+private:
+	IrrlichtHelper irrH_;
 };
 
-void drawPlayfield(IVideoDriver* driver, scene::ISceneManager* smgr, const Configuration& config) {
-	/*smgr->addSkyBoxSceneNode(
-		driver->getTexture(config.getString("textures.skybox_top").c_str()),
-		driver->getTexture(config.getString("textures.skybox_bottom").c_str()),
-		driver->getTexture(config.getString("textures.skybox_left").c_str()),
-		driver->getTexture(config.getString("textures.skybox_right").c_str()),
-		driver->getTexture(config.getString("textures.skybox_front").c_str()),
-		driver->getTexture(config.getString("textures.skybox_back").c_str())
-		);	*/
-		
-	smgr->addSkyDomeSceneNode(driver->getTexture(config.getString("textures.skydome").c_str()));	
-	
-	//bottom 
-	
+void drawPlayfield(IrrlichtHelper irrH, const Configuration& config)
+{
+	//add skydome
+	irrH.smgr->addSkyDomeSceneNode(irrH.driver->getTexture(config.getString("textures.skydome").c_str()));
+
+	//add game borders
+
+	//bottom
 	ISceneNode* bottom = 0;
-	bottom = smgr->addCubeSceneNode(1);
+	bottom = irrH.smgr->addCubeSceneNode(1);
 	bottom->setPosition(core::vector3df(0,-12,0));
 	bottom->setScale(core::vector3df(110,5,110));
-	bottom->setMaterialTexture(0, driver->getTexture(config.getString("textures.bottom").c_str()));
+	bottom->setMaterialTexture(0, irrH.driver->getTexture(config.getString("textures.bottom").c_str()));
 	bottom->setMaterialFlag(video::EMF_LIGHTING, true);
-	
+
 	//sides
-	
-    scene::ISceneNode* side = 0;
-	
-	
-	side = smgr->addCubeSceneNode(1);
+	scene::ISceneNode* side = 0;
+
+	side = irrH.smgr->addCubeSceneNode(1);
 	side->setPosition(core::vector3df(-52.5,-7,0));
 	side->setScale(core::vector3df(5,5,110));
-	side->setMaterialTexture(0, driver->getTexture(config.getString("textures.wall").c_str()));
+	side->setMaterialTexture(0, irrH.driver->getTexture(config.getString("textures.wall").c_str()));
 	side->setMaterialFlag(video::EMF_LIGHTING, true);
 
-	side = smgr->addCubeSceneNode(1);
+	side = irrH.smgr->addCubeSceneNode(1);
 	side->setPosition(core::vector3df(52.5,-7,0));
 	side->setScale(core::vector3df(5,5,110));
-	side->setMaterialTexture(0, driver->getTexture(config.getString("textures.wall").c_str()));
+	side->setMaterialTexture(0, irrH.driver->getTexture(config.getString("textures.wall").c_str()));
 	side->setMaterialFlag(video::EMF_LIGHTING, true);
-	
-	    // add 2 lights
-	bottom = smgr->addLightSceneNode(0, core::vector3df(20,30,40),
-		video::SColorf(1.0f, 0.5f, 0.7f, 1.0f), 100.0f);
 
-	bottom = smgr->addLightSceneNode(0, core::vector3df(20,20,-80),
-		video::SColorf(1.0f, 0.8f, 0.2f, 1.0f), 200.0f);
-		
-		//add ball
-	IAnimatedMesh* bb = smgr->getMesh(config.getString("meshes.ball").c_str());
-	ball = smgr->addAnimatedMeshSceneNode(bb);
-	
+	// add 2 lights to illuminate game board
+	bottom = irrH.smgr->addLightSceneNode(0, core::vector3df(20,30,40),
+	                                      video::SColorf(1.0f, 0.5f, 0.7f, 1.0f), 100.0f);
+
+	bottom = irrH.smgr->addLightSceneNode(0, core::vector3df(20,20,-80),
+	                                      video::SColorf(1.0f, 0.8f, 0.2f, 1.0f), 200.0f);
+
+	//add ball
+	//load animated mesh
+	IAnimatedMesh* bb = irrH.smgr->getMesh(config.getString("meshes.ball").c_str());
+	ball = irrH.smgr->addAnimatedMeshSceneNode(bb);
+
 	ball->setPosition(vector3df(0, -7, 0));
-	ball->setMaterialTexture(0, driver->getTexture(config.getString("textures.ball").c_str()));	
+	ball->setMaterialTexture(0, irrH.driver->getTexture(config.getString("textures.ball").c_str()));
 	ball->setScale(core::vector3df(1,1,1));
 	ball->setMaterialFlag(video::EMF_LIGHTING, false);
+
+	//let ball rotate
+	ballAnim = irrH.smgr->createRotationAnimator(vector3df(2.0, 53, 1.0));
+	ball->addAnimator(ballAnim);
+
+	//add two batters
+	batters.push_back(irrH.smgr->addCubeSceneNode(10));
+
+	batters[0]->setPosition(vector3df(0, -7, -52.5));
+	batters[0]->setMaterialTexture(0, irrH.driver->getTexture(config.getString("textures.batter").c_str()));
+	batters[0]->setScale(vector3df(1.5, 0.5, 0.2));
+	batters[0]->setMaterialFlag(video::EMF_LIGHTING, true);
 	
-	ISceneNodeAnimator* anim = smgr->createRotationAnimator(vector3df(2.0, 53, 1.0));
-	ball->addAnimator(anim);
-	
-	for(int i = 0; i < playerCount; ++i) {
-		batters.push_back(smgr->addCubeSceneNode(10));
-	
-		batters[i]->setPosition(vector3df(0, -7, i == 0 ? -52.5 : 52.5));
-		batters[i]->setMaterialTexture(0, driver->getTexture(config.getString("textures.batter").c_str()));
-		batters[i]->setScale(vector3df(1.5, 0.5, 0.2));
-		batters[i]->setMaterialFlag(video::EMF_LIGHTING, true);
-	}
-	
-	
+	batters.push_back(irrH.smgr->addCubeSceneNode(10));
+
+	batters[1]->setPosition(vector3df(0, -7, 52.5));
+	batters[1]->setMaterialTexture(0, irrH.driver->getTexture(config.getString("textures.batter").c_str()));
+	batters[1]->setScale(vector3df(1.5, 0.5, 0.2));
+	batters[1]->setMaterialFlag(video::EMF_LIGHTING, true);
+
 }
 
-void menu(IrrlichtDevice* device, video::IVideoDriver* driver, gui::IGUIEnvironment* env, scene::ISceneManager* smgr, const Configuration& config, scene::ICameraSceneNode* camera) {
-	
-	IGUISkin* skin = env->getSkin();
-	IGUIFont* font = env->getFont(config.getString("fonts.menu").c_str());
-	if (font)
-		skin->setFont(font);
+void menu(IrrlichtHelper irrH, const Configuration& config)
+{
 
+	//get gui skin and set font
+	IGUISkin* skin = irrH.gui->getSkin();
+	IGUIFont* font = irrH.gui->getFont(config.getString("fonts.menu").c_str());
+	if (font) {
+		skin->setFont(font);
+	}
+	
 	skin->setColor(EGDC_BUTTON_TEXT,video::SColor(255,255,220,100));
 
-	//env->addImage(driver->getTexture(config.getString("images.logo").c_str()), core::position2d<s32>(5,5));
-	
+	//add game logo
+	irrH.gui->addImage(irrH.driver->getTexture(config.getString("images.logo").c_str()), core::position2d<s32>(5,5));
+
 	int width = 450;
 	int height = 60;
-	dimension2d<u32> size = device->getVideoDriver()->getScreenSize();
+	dimension2d<u32> size = irrH.device->getVideoDriver()->getScreenSize();
 	rect<int> pos(10, size.Height - height, 10 + width, size.Height - 10);
-	env->addImage(pos);
-	
-	status = env->addStaticText(L"Irrlicht WOS 06 Demo", pos, true,false);
-	status->setOverrideColor(video::SColor(255,205,200,200));
-	
-	device->getCursorControl()->setVisible(true);
-	
-	int halfScreenWidth = driver->getScreenSize().Width / 2;
-	
-	startButton = env->addButton(rect<s32>(halfScreenWidth-100,210,halfScreenWidth+100,250), 0, START_GAME, L"New Game");
-	exitButton  = env->addButton(rect<s32>(halfScreenWidth-100,290,halfScreenWidth+100,330), 0, EXIT_GAME, L"Quit");
+	irrH.gui->addImage(pos);
 
-	
-	camera = smgr->addCameraSceneNode();
+	//add static stext
+	status = irrH.gui->addStaticText(L"Irrlicht WOS 06 Demo", pos, true,false);
+	status->setOverrideColor(video::SColor(255,205,200,200));
+
+	//rencer cursor visible
+	irrH.device->getCursorControl()->setVisible(true);
+
+	//show menu buttons at correct position
+	int halfScreenWidth = irrH.driver->getScreenSize().Width / 2;
+
+	startButton = irrH.gui->addButton(rect<s32>(halfScreenWidth-100,210,halfScreenWidth+100,250), 0, START_GAME, L"New Game");
+	exitButton  = irrH.gui->addButton(rect<s32>(halfScreenWidth-100,290,halfScreenWidth+100,330), 0, QUIT_GAME, L"Quit");
+
+	//add camera
+	camera = irrH.smgr->addCameraSceneNode();
 	camera->setTarget(vector3df(0,0,0));
-	auto anim = smgr->createFlyCircleAnimator(vector3df(0, 30, -100), 30.0f);
-	camera->addAnimator(anim);
+	camAnim = irrH.smgr->createFlyCircleAnimator(vector3df(0, 30, -100), 30.0f);
+	camera->addAnimator(camAnim);
 }
 
 int main()
 {
+	IrrlichtDevice* device;
+
 	Configuration config;
+	//create irrlicht helper to simplify passing irrlicht objects around
+	IrrlichtHelper irrH;
+	//create event receiver
 	MyEventReceiver eventReceiver;
 
+	//load configuration from file
 	try {
 		config.load("../assets/settings.json");
 
@@ -184,78 +235,81 @@ int main()
 		int fullscreen = config.getInt("general.is_fullscreen");
 
 		device = IrrlichtUtilities::initialize(width, height, fullscreen, &eventReceiver);
-		if(!device) {
-			BOOST_THROW_EXCEPTION(BasicException("aborted creating irrlicht device"));
-		}
 
+		//handle exception in case creation of irrlicht device fails
 	} catch(boost::exception& e) {
 		std::cout<<boost::diagnostic_information(e)<<std::endl;
 		return 1;
 	}
 
+	irrH.device = device;
+	irrH.driver = device->getVideoDriver();
+	irrH.smgr = device->getSceneManager();
+	irrH.gui = device->getGUIEnvironment();
 
-	video::IVideoDriver* driver = device->getVideoDriver();
-	scene::ISceneManager* sceneManager = device->getSceneManager();
-	gui::IGUIEnvironment* guiEnvironment = device->getGUIEnvironment();
-	
-	
-	drawPlayfield(driver, sceneManager, config);	
-	menu(device, driver, guiEnvironment, sceneManager, config, camera);
+	eventReceiver.setIrrlichtHelper(irrH);
+
+	//create converter from irrlicht strings to normal strings and back
+	IrrlichtUtilities::CConverter converter;
+
+	//create pong field
+	drawPlayfield(irrH, config);
+
+	//show menu
+	menu(irrH, config);
+
+	//integer to track fps
 	int lastFPS = -1;
-
 
 	// In order to do framerate independent movement, we have to know
 	// how long it was since the last frame
 	u32 then = device->getTimer()->getTime();
 
-	// This is the movement speed in units per second.
-	const f32 MOVEMENT_SPEED = 50.f;
-
 	while(device->run()) {
 		if(device->isWindowActive()) {
+			
+			//show game information
+			if(gameStarted) {
+				std::ostringstream statusText;
+				statusText<<"Level: "<< 0 <<" Balls: "<< 0;
+				status->setText(converter.strToWchart(statusText.str()));
+			}
 
 			// Work out a frame delta time.
 			const u32 now = device->getTimer()->getTime();
 			const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
 			then = now;
-			/*
-			//move camera according to input
-			core::vector3df pos = root->getPosition();
+
 			
-			if(eventReceiver.isKeyDown(irr::KEY_KEY_W)) {
-				pos.Y += MOVEMENT_SPEED * frameDeltaTime;
-			} else if(eventReceiver.isKeyDown(irr::KEY_KEY_S)) {
-				pos.Y -= MOVEMENT_SPEED * frameDeltaTime;
-			}
-
-			root->setPosition(pos);*/
-
-			driver->beginScene(true, true, video::SColor(255, 200, 200, 200));
-			sceneManager->drawAll();
-			guiEnvironment->drawAll();
-			driver->endScene();
+			//move ball
+			core::vector3df pos = ball->getPosition();
 			
+			pos.X += ballSpeedX * frameDeltaTime;
+			pos.Y += ballSpeedY * frameDeltaTime;
 
-			int fps = driver->getFPS();
+			ball->setPosition(pos);
+
+			irrH.driver->beginScene(true, true, video::SColor(255, 200, 200, 200));
+			irrH.smgr->drawAll();
+			irrH.gui->drawAll();
+			irrH.driver->endScene();
 
 
-			if(lastFPS != fps) {
-				core::stringw str = L"Irrlicht  - Breakout example [";
-				str += driver->getName();
-				str += "] FPS: ";
-				str += fps;
+			int fps = irrH.driver->getFPS();
 
-				device->setWindowCaption(str.c_str());
+			if (lastFPS != fps) {
+				core::stringw tmp(L"Movement Example - Irrlicht Engine [");
+				tmp += irrH.driver->getName();
+				tmp += L"] fps: ";
+				tmp += fps;
+
+				irrH.device->setWindowCaption(tmp.c_str());
 				lastFPS = fps;
 			}
-		} else {
-			device->yield();
 		}
 	}
 
-	device->drop();
-
-
+	irrH.device->drop();
 
 	return 0;
 };
