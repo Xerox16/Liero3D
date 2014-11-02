6cf21fa71db8c8df2b725c31d25374fee1541637
xerox <ert.schneider@yahoo.de>
2014-11-02 10:04:53 +0100

    update debug strings

--- Liero3DCore/entitycomposer.cpp
+++ Liero3DCore/entitycomposer.cpp
@@ -18,6 +18,9 @@ void EntityComposer::registerComposer(const std::string& name, composer func)
 
 std::map<std::string, Entity::ptr> EntityComposer::readXML(const std::string& filename,  EntityCreationContext& context)
 {
+	
+	BOOST_LOG_TRIVIAL(debug)<<"\nEntityComposer::readXML debug";
+	
 	std::map<std::string, Entity::ptr> entities;
 
 	try {
@@ -28,18 +31,18 @@ std::map<std::string, Entity::ptr> EntityComposer::readXML(const std::string& fi
 	//iterate over json and create new entity for every entry
 	for(auto entry = pt.begin(); entry != pt.end(); ++entry) {
 		Entity::ptr entity(new Entity);
-		BOOST_LOG_TRIVIAL(debug)<<"1. level first: "<<entry->first;
+		BOOST_LOG_TRIVIAL(debug)<<"entity: "<<entry->first;
 		
 		//search entry for entity components
 		for(auto component = entry->second.begin(); component != entry->second.end(); ++component) {
 			auto composer = composers_.find(component->first);
-			BOOST_LOG_TRIVIAL(debug)<<"2. level first: "<<entry->first;
+			BOOST_LOG_TRIVIAL(debug)<<"component: "<<component->first;
 		
 			//add them to entity or throw exception if they were not found
 			if(composer != composers_.end()) {
 					composer->second(component->second, *entity, context);
 			} else {
-				BOOST_THROW_EXCEPTION(BasicException("invalid xml entry \"" + component->first + "\""));
+				BOOST_THROW_EXCEPTION(BasicException("no component for xml entry \"" + component->first + "\" defined"));
 			}
 		}
 			entities[entry->first] = entity;
@@ -47,10 +50,13 @@ std::map<std::string, Entity::ptr> EntityComposer::readXML(const std::string& fi
 	
 	//handle exceptions
 	} catch(const boost::property_tree::ptree_bad_path& error) {
-		BOOST_LOG_TRIVIAL(debug)<<"Invalid file: "<<"\""<<filename<<"\" Description: "<<boost::diagnostic_information(error);
+		BOOST_LOG_TRIVIAL(debug)<<"Invalid filename "<<"\""<<filename<<"\" Description: "<<boost::diagnostic_information(error);
 	} catch(const boost::exception& error) {
-		BOOST_LOG_TRIVIAL(debug)<<"Invalid xml structure in file: "<<"\""<<filename<<"\" ! Description: "<<boost::diagnostic_information(error);
+		BOOST_LOG_TRIVIAL(debug)<<"Invalid xml structure in file "<<"\""<<filename<<"\" ! Description: "<<boost::diagnostic_information(error);
 	}
+	
+	
+	BOOST_LOG_TRIVIAL(debug)<<"EntityComposer::readXML debug end \n";
 
 	return entities;
 }
